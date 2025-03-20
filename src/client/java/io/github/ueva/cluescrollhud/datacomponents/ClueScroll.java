9034b1a2af76e518e6108afbd5c9aaaf0fc0bb3b
Joshua Evans <jbe25@bath.ac.uk>
2025-03-20 23:44:48 +0000

    Added records to hold Clue Scroll data.
    About halfway through writing these records, I noticed that Vulengate still uses old-style NBT data on their items, rather than new-style data componenets, so this won't actually be useful for now.
    But, it's nice to have these ready if Vulengate ever decide to update.

---
+++ src/client/java/io/github/ueva/cluescrollhud/datacomponents/ClueScroll.java
@@ -0,0 +1,19 @@
+package io.github.ueva.cluescrollhud.datacomponents;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+import java.util.List;
+
+
+public record ClueScroll(String tier, int created, int expire, List<ClueTask> clues) {
+
+    public static final Codec<ClueScroll> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            Codec.STRING.fieldOf("tier").forGetter(ClueScroll::tier),
+            Codec.INT.fieldOf("amount").forGetter(ClueScroll::created),
+            Codec.INT.fieldOf("completed").forGetter(ClueScroll::expire),
+            ClueTask.CODEC.listOf().fieldOf("clues").forGetter(ClueScroll::clues)
+    ).apply(instance, ClueScroll::new));
+}
+// Note: Currently not used, but prepared in case Vulengate decide to move from old-style NBT to new-style data
+// components.
