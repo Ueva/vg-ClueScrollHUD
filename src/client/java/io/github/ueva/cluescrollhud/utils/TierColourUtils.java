4afc036f832d59180f8fa250edb4a13ab9a8cd35
Joshua Evans <jbe25@bath.ac.uk>
2025-04-10 20:50:21 +0100

    Updated tier colours to match those used on Vulengate.

--- src/client/java/io/github/ueva/cluescrollhud/utils/TierColourUtils.java
+++ src/client/java/io/github/ueva/cluescrollhud/utils/TierColourUtils.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class TierColourUtils {
 
     private static final Map<String, Integer> TIER_COLOURS =
-            Map.of("EASY", 0x55FF55, "NORMAL", 0xAA00AA, "HARD", 0x5555FF, "WEEKLY", 0xAA0000, "EXTENDED", 0xFFAA00);
+            Map.of("EASY", 0x55FF55, "NORMAL", 0xFF55FF, "HARD", 0xFFAA00, "WEEKLY", 0x5555FF, "EXTENDED", 0xFF5555);
 
     private static final int DEFAULT_COLOR = 0xFFFFFF;
 
