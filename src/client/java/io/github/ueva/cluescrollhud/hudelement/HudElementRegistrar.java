ca819c43e551b30267491991ab15ce1d7d07ff18
Joshua Evans <jbe25@bath.ac.uk>
2025-08-31 15:32:02 +0100

    Migrated to new Fabric HUD Element API
    HudLayerRegistrationCallback -> HudElementRegistry
    IdentifiedLayer -> VanillaHudElements

--- src/client/java/io/github/ueva/cluescrollhud/hudelement/HudElementRegistrar.java
+++ src/client/java/io/github/ueva/cluescrollhud/hudelement/HudElementRegistrar.java
@@ -1,8 +1,8 @@
 package io.github.ueva.cluescrollhud.hudelement;
 
 import io.github.ueva.cluescrollhud.VgClueScrollHUD;
-import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
-import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,11 +13,12 @@ public class HudElementRegistrar {
 
     public static void registerHudElements() {
         // Register the ClueScrollHudElement to render on the HUD every frame.
-        HudLayerRegistrationCallback.EVENT.register(layeredDrawer -> layeredDrawer.attachLayerBefore(
-                IdentifiedLayer.CHAT,
+        HudElementRegistry.attachElementBefore(
+                VanillaHudElements.CHAT,
                 ClueScrollHudElement.CLUESCROLL_HUD_LAYER,
                 ClueScrollHudElement::render
-        ));
+        );
+
         LOGGER.info("- Successfully added ClueScrollHudElement to the HUD.");
     }
 }
