8c881824be799e325055a96442d564e051eafe71
Joshua Evans <jbe25@bath.ac.uk>
2026-01-26 01:50:02 +0000

    FIX: Keybind Code Re-Mapping (First Pass)

--- src/client/java/io/github/ueva/cluescrollhud/keybinds/KeybindRegistrar.java
+++ src/client/java/io/github/ueva/cluescrollhud/keybinds/KeybindRegistrar.java
@@ -1,11 +1,16 @@
 package io.github.ueva.cluescrollhud.keybinds;
 
 import io.github.ueva.cluescrollhud.VgClueScrollHUD;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 public class KeybindRegistrar {
+
+    public static final KeyMapping.Category VG_CLUESCROLL_KB_CATEGORY =
+            KeyMapping.Category.register(Identifier.fromNamespaceAndPath(VgClueScrollHUD.MOD_ID, "main"));
 
     private static final Logger LOGGER = LoggerFactory.getLogger(VgClueScrollHUD.MOD_ID);
 
